468feeb5aab584d51de6481854d440885ec7fb5a
Sid Björk <sidricobjork@gmail.com>
2024-10-06 21:40:32 +0200

    add

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the server!' });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+module.exports = app;
